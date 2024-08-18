65739e1c9d06bab08aff848013d8e601a1de290b
Mohamed Abdelrahim <morahim888@gmail.com>
2024-08-18 21:15:51 +0100

    successfully completed week1 project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # week1-project-Spacey
+## Achievements
+The Spacey X website was successfully developed using HTML, CSS, and JavaScript, featuring three main sections.
+### Navigation Bar:
+The navigation bar includes links to different sections of the site. Smooth scrolling and hover effects were implemented for all internal links, allowing seamless navigation across the page.
+### Main Sections:
+Background images, flexbox, and a responsive layout were used to create visually appealing content sections. A "back to top" button was added, enabling users to quickly return to the top of the page.
+### Footer:
+A basic footer was created, featuring social media icons that link to external social media sites.
+Background Audio:
+Background music plays automatically when users interact with the website, adding to the immersive experience.
+## Challenges
+1. Footer Styling
+Achieving consistent footer styling across various screen sizes was challenging, particularly in aligning it with the background image.
+2. Autoplay Background Music
+Implementing autoplay for background music was difficult due to browser restrictions. A JavaScript solution was used to ensure the music plays after the user interacts with the website.
+## Future Enhancements
+Advanced Footer Features: Plan to enhance the footer with additional content and features.
+Music Controls: Implement user controls for the background music.
--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 
+                    //For Navigation Bar 
+// To go to the second page when user clicked in tickets at nav bar 
+document.querySelector("#tickets").addEventListener("click", function() {
+    document.querySelector(".section-two").scrollIntoView({ behavior: "smooth"});});
+
+// To go to the second page when user clicked in rockets at nav bar 
+document.querySelector("#rockets").addEventListener("click", function() {
+    document.querySelector(".section-two").scrollIntoView({ behavior: "smooth"});});
+
+// To go to the second page when user clicked in pricing at nav bar 
+document.querySelector("#pricing").addEventListener("click", function() {
+    document.querySelector(".section-three").scrollIntoView({ behavior: "smooth"});});
+
+
+                //For Pages Button
 // To scroll to the second page when the (NEXT) button is clicked
 document.querySelector(".page1-button").addEventListener("click", function() {
-    document.querySelector(".section-two").scrollIntoView({ behavior: "smooth"});
-});
+    document.querySelector(".section-two").scrollIntoView({ behavior: "smooth"})});
 
 // To scroll to the third page when the (NEXT) button is clicked
 document.querySelector(".page2-button").addEventListener("click", function() {
-    document.querySelector("footer").scrollIntoView({ behavior: "smooth" });
-});
+    document.querySelector("footer").scrollIntoView({ behavior: "smooth" });});
 
 // TO scroll to the top page when the (TOP) button is clicked
 document.querySelector(".page3-button").addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: "smooth"});
-});
+    window.scrollTo({ top: 0, behavior: "auto"});});
 
-
-    // To paly backgroung music when user clicked on the page
-    document.addEventListener("click", function() {
+            //For Background Music
+// To paly backgroung music when user clicked on the page
+document.addEventListener("click", function() {
         let audio = document.querySelector(".background-music");
-        if (audio.paused) {
-            audio.play();
-        }
-    });
+        if (audio.paused) { audio.play(); }});
